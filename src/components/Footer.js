0b74bcc173fb37450f2be36e754f14a44942189b
DevJamshid <devjamshid@gmail.com>
2021-02-13 05:08:17 +0900

    This is Keeper-app, made using ReactJs

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function Footer() {
+    const currentTime = new Date().getFullYear();
+  return (
+    <footer>
+      <p>Copyright ©️ {currentTime}</p>
+    </footer>
+  );
+}
+
+export default Footer;
